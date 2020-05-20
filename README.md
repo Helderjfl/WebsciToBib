c4d752c27cd73bfa6fb4a0028a303929cd412e27
Helder Jefferson <Helderjfl@users.noreply.github.com>
2020-05-19 21:24:57 -0300

    Modified script to solve bug
    Solving bug for opening CSV file and working with correct encoding.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WebsciToBib
-converter CSV do Web of Science e do Springer para bibtex
+Python script to convert Springer and Web of Science CSV to bibtex format.
+
+##How to use
+> python SpringerCSVtoBib.py \<csv file name> \<bibtex file name>
--- SpringerCSVtoBib.py
+++ SpringerCSVtoBib.py
@@ -5,12 +5,13 @@ import sys
 file = sys.argv[1]
 bibFile = sys.argv[2]
 
-with open(file,'rb') as csvfile:
+with open(file,'rt', encoding="UTF8") as csvfile:
 	texto = '% Encoding: UTF-8\n\n'
 	spamreader = csv.reader(csvfile,delimiter=',')		
 	i = 1;
+
 	for row in spamreader:				
 		texto += '@article{Springer' + str(i) + ',\n' + '\t' + 'author = {' + row[6] + '},\n' +  '\t' + 'title = {' + row[0] + '},\n' + '\t' + 'journal = {' + row[1] + '},\n' +'\t' + 'year = {' + row[7] + '},\n' +	'}\n\n'
 		i += 1
-with open(bibFile,'a') as arq:
+with open(bibFile,'a', encoding="UTF8") as arq:
 	arq.write(texto)	
